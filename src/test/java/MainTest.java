a2105eb9ff91153291e3cdda916245686c42079a
dimonhouse <B8ig35uqzxv17p6i>
2023-05-18 19:40:17 +0300

    fix

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -14,8 +14,8 @@ public class MainTest {
     public void test1(){
         browserCapabilities.setCapability("enableVNC", true);
         browserSize = "1920x1080";
-        browser = "firefox";
-        Configuration.remote = "http://ec2-54-191-83-209.us-west-21.compute.amazonaws.com:4444/wd/hub";
+        //browser = "firefox";
+        //Configuration.remote = "http://ec2-54-191-83-209.us-west-21.compute.amazonaws.com:4444/wd/hub";
 
 
         open("https://www.google.com/");
